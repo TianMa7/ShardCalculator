da3ee9ef5ee8ececf059a2e5bdd0d9e6bede8bcd
tianm <tianma787@gmail.com>
2025-07-29 19:52:42 -0600

    changed shardcalculator to process cheapest shards

--- shardCalculator.py
+++ shardCalculator.py
@@ -4,10 +4,26 @@ def shardCalculator(shardList:dict, shard: dict, buyMethod: str, sellMethod: str
     #shardlist is bazaar data
     id = shard["id"]
     prereq = shard["fusion_sources"]
+    
+
+
+
 
     chamPrereqData = []
     basePrereqData = []
     chamData = []
+    cheapestShards = [{
+        "productId": "SHARD_MEGALITH",
+        "sellPrice": float('inf'),
+        "sellVolume": 422,
+        "sellMovingWeek": 8259,
+        "sellOrders": 27,
+        "buyPrice": float('inf'),
+        "buyVolume": 103389,
+        "buyMovingWeek": 19782,
+        "buyOrders": 956,
+        "shardId": "L29"
+    }for _ in range(5)]
 
     for item in prereq["base"]:
         for item2 in shardList:
@@ -25,6 +41,27 @@ def shardCalculator(shardList:dict, shard: dict, buyMethod: str, sellMethod: str
         if item["shardId"] == "L4":
             chamData.append(item)
             break  
+        if item["shardId"][0] in ["C","U","R","E","L"] and item[buyMethod] < cheapestShards[0][buyMethod]:
+            cheapestShards[0] = item
+        if item["shardId"][0] in ["U","R","E","L"] and item[buyMethod] < cheapestShards[1][buyMethod]:
+            cheapestShards[0] = item
+        if item["shardId"][0] in ["R","E","L"] and item[buyMethod] < cheapestShards[2][buyMethod]:
+            cheapestShards[0] = item
+        if item["shardId"][0] in ["E","L"] and item[buyMethod] < cheapestShards[3][buyMethod]:
+            cheapestShards[0] = item
+        if item["shardId"][0] in ["L"] and item[buyMethod] < cheapestShards[4][buyMethod]:
+            cheapestShards[0] = item
+            
+
+
+
+            
+
+
+
+
+
+
     
 
 
@@ -60,7 +97,6 @@ def chameleonCalc(prereqData: dict, buyMethod: str, sellMethod: str, chamData: d
         price = (item["fusion_quantity"] * item[buyMethod]) + (2 * chamData[buyMethod])
         returnOptions.append({"price": price, "id1": item, "id2": chamData})
     
-    #how do i make this return only the cheapest option
     if returnOptions:
         return min(returnOptions, key=lambda x: x["price"])
     else:
@@ -74,12 +110,28 @@ def chameleonCalc(prereqData: dict, buyMethod: str, sellMethod: str, chamData: d
 #must change all commented out lines
 #find a way to get a list of data of cheapest shard of each category(fodder shards)
 
-def baseCalc(prereqData: dict, buyMethod: str, sellMethod: str, chamData: dict) -> dict:
+def baseCalc(prereqData: dict, buyMethod: str, sellMethod: str, cheapItem: dict) -> dict:
+    #TODO: create a seperate function that creates dict cheapItem, which has a dictionary of the cheapest shard in each rarity
+
+
+
+
+
     returnOptions = []
 
     for item in prereqData:
-        # price = (item["fusion_quantity"] * item[buyMethod]) + (2 * chamData[buyMethod])
-        # returnOptions.append({"price": price, "id1": item, "id2": chamData})
+        if item["shardId"][0] == "C":
+            rarity = 0
+        if item["shardId"][0] == "U":
+            rarity = 1
+        if item["shardId"][0] == "R":
+            rarity = 2
+        if item["shardId"][0] == "E":
+            rarity = 3
+        if item["shardId"][0] == "L":
+            rarity = 4
+        price = (item["fusion_quantity"] * item[buyMethod]) + (cheapItem[rarity]["fusion_quantity"] * cheapItem[rarity][buyMethod])
+        returnOptions.append({"price": price, "id1": item, "id2": cheapItem[rarity]})
     
     #how do i make this return only the cheapest option
     if returnOptions:
